e334266be20ff118ee00ca8fe1afa5fda713d5f2
Gia Hau <nguyengiahau2004@gmail.com>
2024-03-01 16:23:30 +0700

    01-03-2024

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+    return (
+        <div className='descriptionbox'>
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>The world of e-commerce is booming, with online shopping experiences increasingly shaping our consumer landscape. From its humble beginnings to its current sophisticated state, e-commerce has revolutionized the way we buy and sell goods. Its convenience, diverse offerings, and accessibility have not only benefitted consumers through wider choice and competitive pricing, but also empowered businesses with wider reach and efficient operations. However, challenges remain in the form of cybersecurity concerns, logistics intricacies, and building trust online. As e-commerce continues to evolve, with trends like AI personalization and immersive reality experiences emerging, its impact on our economic and social fabric promises to be even more significant.</p>
+                <p>
+                    The world of e-commerce is booming, with online shopping experiences increasingly shaping our consumer landscape. From its humble beginnings to its current sophisticated state, e-commerce has revolutionized the way we buy and sell goods. Its convenience, diverse offerings, and accessibility have not only benefitted consumers through wider choice and competitive pricing, but also empowered businesses with wider reach and efficient operations. However, challenges remain in the form of cybersecurity concerns, logistics intricacies, and building trust online. As e-commerce continues to evolve, with trends like AI personalization and immersive reality experiences emerging, its impact on our economic and social fabric promises to be even more significant. This begs the question: what does the future hold for e-commerce, and how can we adapt and thrive in its ever-changing landscape?</p>
+            </div>
+        </div>
+    )
+}
+
+export default DescriptionBox
